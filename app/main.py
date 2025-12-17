264abdc1832650b764e0f8eb4b91de6a936e5427
drelazor333-glitch <drelazor333@gmail.com>
2025-12-17 19:50:54 +0300

    Ваше сообщение о коммите

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-#  write your code here
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self):
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self):
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return selff.appetite
+        return 0
